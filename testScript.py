6546aeef1c4d42ccf32df500c2a7cf3cc8ed4975
Ben Van Citters macbooc air <van.citters.b@gmail.com>
2022-01-27 00:44:14 -0600

    parsing more json, adding polyline lib

--- testScript.py
+++ testScript.py
@@ -1,16 +1,49 @@
 import http.client
 import json
+import polyline
 
-conn = http.client.HTTPSConnection("www.strava.com")
-payload = ''
-headers = {
-  'Authorization': 'Bearer 9b8412bcb3be47dca11818cd2b9a06a5b2b20ebf'
-}
-conn.request("GET", "/api/v3/athlete/activities", payload, headers)
-res = conn.getresponse()
-data = res.read()
 
-str_data = data.decode("utf-8")
-resp_data_json = json.loads(str_data)
-for activity in resp_data_json:
-    print(str(activity['id']) + ' - ' + str(activity['name']) + ' - ' + str(activity['type']))
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def doAllStravaStuff():
+    conn = http.client.HTTPSConnection("www.strava.com")
+    payload = ''
+    headers = {
+      'Authorization': 'Bearer 99e31cb7683695c133429b0397bec240d4535533'
+    }
+    conn.request("GET", "/api/v3/athlete/activities", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    str_data = data.decode("utf-8")
+    resp_data_json = json.loads(str_data)
+    # print(resp_data_json)
+    mything = []
+    for activity in resp_data_json:
+        # print(activity)
+        # print(activity['map'])
+
+        polyl = polyline.decode(activity['map']['summary_polyline'])
+        print(f'poly: {polyl}')
+        mything = polyl
+        #https://developers.google.com/maps/documentation/utilities/polylinealgorithm
+        # print( str(activity['id'] ) + ' - ' + str(activity['name']) + ' - ' + str(activity['type']))
+        # map': {'id': 'a6585569005', 'summary_polyline'
+        break
+        # conn.request("GET", f"/activities/{activity['id']}", payload, headers)
+        # act_res = conn.getresponse()
+        # act_data = act_res.read()
+        #
+        # str_act_data = act_data.decode("utf-8")
+        # resp_actjson = json.loads(str_act_data)
+    return mything
+
+
+@app.route("/")
+def hello_world():
+    s = doAllStravaStuff()
+    return f'<p>hello {s}</p>'
+
